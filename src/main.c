76c777d07607665a516658f07c0386c85d231356
DavaRamadhann <davaramadhan561@gmail.com>
2025-03-13 10:10:48 +0700

    Update

--- src/main.c
+++ src/main.c
@@ -1,26 +1,40 @@
 #include "raylib.h"
+#include "dava.h"
 #include "zakky.h"
 
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 450
+#define MAX_BIRDS 1
+
 int main() {
-    const int screenWidth = 800;
-    const int screenHeight = 600;
-    
-    InitWindow(screenWidth, screenHeight, "Flappy Bird with Image");
-
-    // Buat burung di tengah layar dengan skala 0.4 (40% ukuran aslinya)
-    Bird bird = CreateBird(screenWidth / 6, screenHeight / 2, "flappy.png", 0.4f);
-
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Flappy Bird - Combined Version");
     SetTargetFPS(60);
+
+    // Inisialisasi burung menggunakan metode dari bird.c
+    Bird birds[MAX_BIRDS];
+    InitBirds(birds, MAX_BIRDS);
+    
+    // Inisialisasi burung menggunakan metode dari birdd.c
+    Bird bird = CreateBird(SCREEN_WIDTH / 8, SCREEN_HEIGHT / 2, "Flappy.png", 0.8f);
     
     while (!WindowShouldClose()) {
+        UpdateBirds(birds, MAX_BIRDS); // Update gerakan burung dari bird.c
+        
         BeginDrawing();
         ClearBackground(SKYBLUE);
-        DrawBird(bird);
+        
+        // Gambar burung dari bird.c
+        DrawBirds(birds, MAX_BIRDS);
+        
+        // Gambar burung dari birdd.
+        
+        DrawText("Press SPACE to Flap!", 10, 10, 20, DARKGRAY);
         EndDrawing();
     }
 
-    UnloadBird(&bird);
+    UnloadBirds(birds, MAX_BIRDS); // Bersihkan memori burung dari bird.c
+    UnloadBird(&bird); // Bersihkan memori burung dari birdd.c
     CloseWindow();
-
+    
     return 0;
 }
