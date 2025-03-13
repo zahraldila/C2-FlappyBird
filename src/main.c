c19543fe310d17774bb03670d851b8753aeba4e8
DavaRamadhann <davaramadhan561@gmail.com>
2025-03-13 15:10:10 +0700

    Update penambahan background

--- src/main.c
+++ src/main.c
@@ -11,38 +11,55 @@
 int main() {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Flappy Bird - Combined Version");
     SetTargetFPS(60);
-    
+
+    // Variabel untuk posisi background
+    float bgX = 0;
+    Texture2D cityBg = LoadTexture("city.png");
+
     GameState currentState = MENU;
 
     Bird birds[MAX_BIRDS];
     InitBirds(birds, MAX_BIRDS);
     Bird bird = CreateBird(SCREEN_WIDTH / 8, SCREEN_HEIGHT / 2, "Flappy.png", 0.8f);
-    
+
     int Pipa[3][3];
     Buat_pipa(Pipa);
-    
+
     while (!WindowShouldClose()) {
-        UpdateBirds(birds, MAX_BIRDS);
-        Pergerakan_pipa(Pipa);
-        
+        // Update background position
+        bgX -= 0.5f;
+        if (bgX <= -SCREEN_WIDTH) {
+            bgX = 0;
+        }
+
+        BeginDrawing();
+        ClearBackground(SKYBLUE);
+
+        // Gambar background di belakang
+        DrawBackground(cityBg, bgX);
+
         if (currentState == MENU) {
             currentState = DrawMenu(SCREEN_WIDTH, SCREEN_HEIGHT);
         } 
         else if (currentState == GAMEPLAY) {
-        BeginDrawing();
-        ClearBackground(SKYBLUE);
-        DrawBirds(birds, MAX_BIRDS);
-        
-        Gambar_pipa(Pipa);
-        DrawText("Press SPACE to Flap!", 10, 10, 20, DARKGRAY);
+            // Update objek game
+            UpdateBirds(birds, MAX_BIRDS);
+            Pergerakan_pipa(Pipa);
+
+            // Gambar objek game
+            DrawBirds(birds, MAX_BIRDS);
+            Gambar_pipa(Pipa);
+            DrawText("Press SPACE to Flap!", 10, 10, 20, DARKGRAY);
         }
 
         EndDrawing();
     }
-    
+
+    // Unload assets
     UnloadBirds(birds, MAX_BIRDS);
     UnloadBird(&bird);
+    UnloadTexture(cityBg);
     CloseWindow();
-    
+
     return 0;
 }
