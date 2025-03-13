b7e4c5e6bf4b5e3cf9a283f2002b5c83bc32edc8
DavaRamadhann <davaramadhan561@gmail.com>
2025-03-13 13:39:34 +0700

    Update file main.c untuk menjalankan program
    main.c

--- src/main.c
+++ src/main.c
@@ -1,37 +1,48 @@
+// File: main.c
 #include "raylib.h"
+#include "dava.h"
 #include "zakky.h"
+#include "alexandrio.h"
+
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 450
+#define MAX_BIRDS 1
 
 int main() {
-    const int screenWidth = 800;
-    const int screenHeight = 600;
-    
-    InitWindow(screenWidth, screenHeight, "Flappy Bird with Menu");
-
-    GameState currentState = MENU;
-
-    // Buat burung di tengah dan sedikit ke kiri
-    float birdX = screenWidth * 0.3f;
-    float birdY = screenHeight / 2;
-    Bird bird = CreateBird(birdX, birdY, "flappy.png", 0.4f);
-
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Flappy Bird - Combined Version");
     SetTargetFPS(60);
     
-    while (!WindowShouldClose()) {
-        BeginDrawing();
-        ClearBackground(SKYBLUE);
+    GameState currentState = MENU;
 
+    Bird birds[MAX_BIRDS];
+    InitBirds(birds, MAX_BIRDS);
+    Bird bird = CreateBird(SCREEN_WIDTH / 8, SCREEN_HEIGHT / 2, "Flappy.png", 0.8f);
+    
+    int Pipa[3][3];
+    Buat_pipa(Pipa);
+    
+    while (!WindowShouldClose()) {
+        UpdateBirds(birds, MAX_BIRDS);
+        Pergerakan_pipa(Pipa);
+        
         if (currentState == MENU) {
-            currentState = DrawMenu(screenWidth, screenHeight);
+            currentState = DrawMenu(SCREEN_WIDTH, SCREEN_HEIGHT);
         } 
         else if (currentState == GAMEPLAY) {
-            DrawBird(bird);
+        BeginDrawing();
+        ClearBackground(SKYBLUE);
+        DrawBirds(birds, MAX_BIRDS);
+        
+        Gambar_pipa(Pipa);
+        DrawText("Press SPACE to Flap!", 10, 10, 20, DARKGRAY);
         }
 
         EndDrawing();
     }
-
+    
+    UnloadBirds(birds, MAX_BIRDS);
     UnloadBird(&bird);
     CloseWindow();
-
+    
     return 0;
 }
