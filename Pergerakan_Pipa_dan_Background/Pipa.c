5372be53447d15858def597dcb5a3dfda0fdaf64
Alexandrio Vega Bonito <alexandriovegabonito@gmail.com>
2025-03-13 09:39:35 +0700

    Menambah desain pipa biar lebih bagus

--- Pergerakan_Pipa_dan_Background/Pipa.c
+++ Pergerakan_Pipa_dan_Background/Pipa.c
@@ -12,31 +12,42 @@ Color RUMPUT = {0, 150, 0 , 255};
 
 
 
-void Buat_pipa(int Pipa[3][3]){
+void Buat_pipa(int Pipa[3][3], int TutupPipa[3][3]){
     for(int i = 0; i < 3; i++){
         Pipa[i][0] = (LEBAR_LAYAR + i * 300) + 10;
         Pipa[i][1] = rand() % (TINGGI_LAYAR - JARAK_PIPA_ATAS_BAWAH - 150) + 50;
         Pipa[i][2] = 0;
+        TutupPipa[i][0] = (LEBAR_LAYAR + i * 300);
+        TutupPipa[i][1] = Pipa[i][2];
+        TutupPipa[i][2] = 0;
     }   
 }
 
 
-void Pergerakan_pipa(int Pipa[3][3]){
+void Pergerakan_pipa(int Pipa[3][3], int TutupPipa[3][3]){
     for(int i = 0; i < 3; i++){
         Pipa[i][0] -= KECEPATAN_PIPA;
+        TutupPipa[i][0] -= KECEPATAN_PIPA;
         if(Pipa[i][0] + LEBAR_PIPA < 0){
             Pipa[i][0] = LEBAR_LAYAR + 20;
             Pipa[i][1] = rand() % (TINGGI_LAYAR - JARAK_PIPA_ATAS_BAWAH - 100) + 50;
             Pipa[i][2] = 0;   
         }
+        if(TutupPipa[i][0] + LEBAR_PIPA + 20 < 0){
+            TutupPipa[i][0] = LEBAR_LAYAR;
+            TutupPipa[i][1] = Pipa[i][2];
+            TutupPipa[i][2] = 0;
+        }
     }
 }
 
 
-void Gambar_pipa(int Pipa[3][3]){
+void Gambar_pipa(int Pipa[3][3], int TutupPipa[3][3]){
     for(int i = 0; i < 3; i++){
         DrawRectangle(Pipa[i][0], 0, LEBAR_PIPA, Pipa[i][1], GREEN);
+        DrawRectangle(TutupPipa[i][0], Pipa[i][1] - 30, LEBAR_PIPA + 20, 30, GREEN);
         DrawRectangle(Pipa[i][0], Pipa[i][1] + JARAK_PIPA_ATAS_BAWAH, LEBAR_PIPA, TINGGI_LAYAR - Pipa[i][1] - JARAK_PIPA_ATAS_BAWAH, GREEN);
+        DrawRectangle(TutupPipa[i][0], Pipa[i][1] + JARAK_PIPA_ATAS_BAWAH, LEBAR_PIPA + 20, 30, GREEN);
         DrawRectangle(0, 400, LEBAR_LAYAR, 100, TANAH);
         DrawRectangle(0, 400, LEBAR_LAYAR, 15, RUMPUT);
     }
@@ -47,14 +58,14 @@ int main() {
     InitWindow(LEBAR_LAYAR, TINGGI_LAYAR, "Flappy Bird Pipes");
     SetTargetFPS(60);
     srand(time(NULL));
-    int Pipa[3][3];      
-    Buat_pipa(Pipa);
+    int Pipa[3][3], TutupPipa[3][3];      
+    Buat_pipa(Pipa, TutupPipa);
     // cukup Pipa tidak perlu Pipa[3][3] karena sudah mengirim alamat array
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(SKYBLUE);
-        Pergerakan_pipa(Pipa);   
-        Gambar_pipa(Pipa);
+        Pergerakan_pipa(Pipa, TutupPipa);   
+        Gambar_pipa(Pipa, TutupPipa);
         EndDrawing();
     }
     
