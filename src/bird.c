c464714a92d4d369f57ea51df928b5e494fde27f
Alexandrio Vega Bonito <alexandriovegabonito@gmail.com>
2025-03-14 19:42:43 +0700

    Mengatur agar Program pipa (Alexandrio) terdapat di src dan menghapus program pergerakan pipa dari folder pergerakan pipa

--- src/bird.c
+++ src/bird.c
@@ -2,7 +2,6 @@
 #include "raylib.h"
 #include "dava.h"
 #include "zakky.h"
-#include "alexandrio.h"
 #include "bird_struct.h"
 #include <stdlib.h>
 #include <time.h>
@@ -75,45 +74,7 @@ void UnloadBird(Bird *bird) {
     UnloadTexture(bird->texture);
 }
 
-// Implementasi dari alexandrio.h (pipa)
-void Buat_pipa(int Pipa[3][3], int TutupPipa[3][3]){
-    for(int i = 0; i < 3; i++){
-        Pipa[i][0] = (SCREEN_WIDTH * 300) + 10;
-        Pipa[i][1] = rand() % (SCREEN_HEIGHT - JARAK_PIPA_ATAS_BAWAH - 150) + 50;
-        Pipa[i][2] = 0;
-        TutupPipa[i][0] = (SCREEN_WIDTH + i * 300);
-        TutupPipa[i][1] = Pipa[i][2];
-        TutupPipa[i][2] = 0;
-    }   
-}
 
-void Pergerakan_pipa(int Pipa[3][3], int TutupPipa[3][3]){
-    for(int i = 0; i < 3; i++){
-        Pipa[i][0] -= KECEPATAN_PIPA;
-        TutupPipa[i][0] -= KECEPATAN_PIPA;
-        if(Pipa[i][0] + LEBAR_PIPA < 0){
-            Pipa[i][0] = SCREEN_WIDTH + 20;
-            Pipa[i][1] = rand() % (SCREEN_HEIGHT - JARAK_PIPA_ATAS_BAWAH - 100) + 50;
-            Pipa[i][2] = 0;   
-        }
-        if(TutupPipa[i][0] + LEBAR_PIPA + 20 < 0){
-            TutupPipa[i][0] = SCREEN_WIDTH;
-            TutupPipa[i][1] = Pipa[i][2];
-            TutupPipa[i][2] = 0;
-        }
-    }
-}
-
-void Gambar_pipa(int Pipa[3][3], int TutupPipa[3][3]){
-    for(int i = 0; i < 3; i++){
-        DrawRectangle(Pipa[i][0], 0, LEBAR_PIPA, Pipa[i][1], GREEN);
-        DrawRectangle(TutupPipa[i][0], Pipa[i][1] - 30, LEBAR_PIPA + 20, 30, GREEN);
-        DrawRectangle(Pipa[i][0], Pipa[i][1] + JARAK_PIPA_ATAS_BAWAH, LEBAR_PIPA, SCREEN_HEIGHT - Pipa[i][1] - JARAK_PIPA_ATAS_BAWAH, GREEN);
-        DrawRectangle(TutupPipa[i][0], Pipa[i][1] + JARAK_PIPA_ATAS_BAWAH, LEBAR_PIPA + 20, 30, GREEN);
-        DrawRectangle(0, 400, SCREEN_WIDTH, 100, TANAH);
-        DrawRectangle(0, 400, SCREEN_WIDTH, 15, RUMPUT);
-    }
-}
 
 GameState DrawMenu(int screenWidth, int screenHeight) {
     GameState state = MENU;
