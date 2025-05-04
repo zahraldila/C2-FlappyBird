92346e8c479a0d18eb18c3fdca0f61293ae2f8e9
DavaRamadhann <davaramadhan561@gmail.com>
2025-05-04 18:11:00 +0700

    Add files via upload

--- include/dava.h
+++ include/dava.h
@@ -8,11 +8,12 @@
 #define GRAVITY 0.5f
 #define FLAP_STRENGTH -8.0f
 
-// Deklarasi fungsi
-void InitBirds(Bird bird[], int count);
-void UpdateBirds(Bird bird[], int count);
-void DrawBirds(Bird bird[], int count);
-void UnloadBirds(Bird bird[], int count);
+// Fungsi untuk linked list
+BirdNode* InitBirdsLinkedList(int count);
+void UpdateBirds(BirdNode *head);
+void DrawBirds(BirdNode *head);
+void UnloadBirds(BirdNode *head);
+
 void InitBackground(Texture2D *bg);
 void UpdateBackground(float *bgX);
 void DrawBackground(Texture2D bg, float bgX);
