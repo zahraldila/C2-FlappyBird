ac83dfa08ef36a50ceab5a9bcc34b0b70902fef4
DavaRamadhann <davaramadhan561@gmail.com>
2025-05-04 18:13:27 +0700

    Perubahan struktur data bird menjadi linked list

--- src/main.c
+++ src/main.c
@@ -1,4 +1,3 @@
-// File: main.c
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -8,7 +7,7 @@
 #include "alexandrio.h"
 #include "zahra.h"
 #include "qlio.h"
-#include "sound.h"  
+#include "sound.h"
 
 int main() {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Flappy Bird");
@@ -19,7 +18,7 @@ int main() {
     SetRandomSeed(time(NULL));
 
     // Background
-    bgX = 0;
+    float bgX = 0;
     Texture2D cityBg = LoadTexture("city.png");
 
     GameState currentState = MENU;
@@ -27,9 +26,10 @@ int main() {
     PauseState tmblpause;
     jedapause(&tmblpause);
 
-    Bird birds[MAX_BIRDS];
-    InitBirds(birds, MAX_BIRDS);
-    Bird bird = CreateBird(SCREEN_WIDTH / 3, SCREEN_HEIGHT / 2, "Flappy.png", 0.8f); // posisi burung agak kanan
+    // Burung menggunakan Singly Linked List
+    BirdNode *birdList = InitBirdsLinkedList(MAX_BIRDS);
+    birdList->bird.position.y = SCREEN_HEIGHT / 2;  // posisi burung agak kanan
+    birdList->bird.speed = 0;
 
     Buat_awan(Awan);
     Buat_pipa(Pipa, TutupPipa);
@@ -40,24 +40,22 @@ int main() {
 
     // Inisialisasi sistem skor
     InitSkor();
-    
+
     // === Inisialisasi Sound ===
     InitAudioDevice();
     InitSounds();
-    
+
     // Flag untuk mengelola menu music
     bool menuMusicStarted = false;
 
     while (!WindowShouldClose()) {
-        if (gameOverState != GAME_OVER && IsKeyPressed(KEY_P)) 
-        {
+        if (gameOverState != GAME_OVER && IsKeyPressed(KEY_P)) {
             tombolpause(&tmblpause);
         }
 
         UpdateMusic();  // Update music stream
 
-        if (!tmblpause.isPause) 
-        {
+        if (!tmblpause.isPause) {
             // Background bergerak
             Pergerakan_awan(Awan);
             bgX -= 0.5f;
@@ -71,44 +69,40 @@ int main() {
 
         if (currentState == MENU) {
             gameOverState = GAME_READY;
-            
-            // Hanya memulai musik menu sekali ketika memasuki state menu
+
             if (!menuMusicStarted) {
                 PlayMenuMusic();
                 menuMusicStarted = true;
             }
-            
+
             currentState = DrawMenu(SCREEN_WIDTH, SCREEN_HEIGHT);
 
             if (currentState == GAMEPLAY) {
-                StopMenuMusic(); // Stop musik saat masuk gameplay
-                menuMusicStarted = false; // Reset flag
+                StopMenuMusic();
+                menuMusicStarted = false;
 
-                birds[0].position.y = SCREEN_HEIGHT / 2;
-                birds[0].speed = 0;
+                birdList->bird.position.y = SCREEN_HEIGHT / 2;
+                birdList->bird.speed = 0;
                 Buat_pipa(Pipa, TutupPipa);
-                score = 0; // Gunakan variabel global dari qlio.h
+                score = 0;
                 scoreSaved = false;
                 for (int i = 0; i < 3; i++) passedPipe[i] = false;
             }
         } else if (currentState == GAMEPLAY) {
             if (!tmblpause.isPause) {
                 if (gameOverState == GAME_READY) {
-                    birds[0].position.y = SCREEN_HEIGHT / 2;
-                    birds[0].speed = 0;
+                    birdList->bird.position.y = SCREEN_HEIGHT / 2;
+                    birdList->bird.speed = 0;
 
                     if (IsKeyPressed(KEY_SPACE)) {
                         gameOverState = GAME_ACTIVE;
-                        birds[0].speed = FLAP_STRENGTH;
+                        birdList->bird.speed = FLAP_STRENGTH;
                         PlaySoundEffect(SOUND_FLAP);
                     }
                 } else if (gameOverState == GAME_ACTIVE) {
-                    // Update posisi burung
-                    UpdateBirds(birds, MAX_BIRDS);
-                    
-                    // Update posisi pipa
+                    UpdateBirds(birdList);
                     Pergerakan_pipa(Pipa, TutupPipa);
-                    
+
                     for (int i = 0; i < 3; i++) {
                         if (Pipa[i][0] > LEBAR_LAYAR && passedPipe[i]) {
                             passedPipe[i] = false;
@@ -116,9 +110,8 @@ int main() {
                     }
 
                     GameOverState prevState = gameOverState;
-                    gameOverState = UpdateGameCollision(birds[0], Pipa, TutupPipa);
+                    gameOverState = UpdateGameCollision(birdList->bird, Pipa, TutupPipa);
 
-                    // Cek collision sound
                     if (prevState == GAME_ACTIVE && gameOverState == GAME_OVER) {
                         PlaySoundEffect(SOUND_COLLIDE);
                         PlaySoundEffect(SOUND_GAME_OVER);
@@ -126,13 +119,12 @@ int main() {
                     }
 
                     if (gameOverState == GAME_ACTIVE) {
-                        float birdRightX = birds[0].position.x + birds[0].texture.width;
+                        float birdRightX = birdList->bird.position.x + birdList->bird.texture.width;
 
                         for (int i = 0; i < 3; i++) {
                             float pipeRightX = Pipa[i][0] + LEBAR_PIPA;
 
                             if (!passedPipe[i] && birdRightX > pipeRightX) {
-                                // Gunakan fungsi TambahSkor untuk menambah score dan update highscore
                                 TambahSkor();
                                 PlaySoundEffect(SOUND_SCORE);
                                 passedPipe[i] = true;
@@ -142,41 +134,37 @@ int main() {
                         }
 
                         if (IsKeyPressed(KEY_SPACE)) {
-                            birds[0].speed = FLAP_STRENGTH;
+                            birdList->bird.speed = FLAP_STRENGTH;
                             PlaySoundEffect(SOUND_FLAP);
                         }
                     }
-                } else { // GAME_OVER
+                } else {
                     if (!scoreSaved) {
-                        // Simpan highscore hanya sekali saat game over
                         SimpanHighscore();
                         Pipa_berhenti(false);
                         scoreSaved = true;
                     }
-                    
+
                     if (IsKeyPressed(KEY_ENTER)) {
                         gameOverState = GAME_READY;
                         Pipa_berhenti(true);
-                        ResetGame(&birds[0], Pipa, TutupPipa);
-                        ResetSkor(); // Gunakan fungsi reset skor
+                        ResetGame(&birdList->bird, Pipa, TutupPipa);
+                        ResetSkor();
                         scoreSaved = false;
                         for (int i = 0; i < 3; i++) passedPipe[i] = false;
                     } else if (IsKeyPressed(KEY_BACKSPACE)) {
                         currentState = MENU;
-                        ResetGame(&birds[0], Pipa, TutupPipa);
-                        ResetSkor(); // Gunakan fungsi reset skor
+                        ResetGame(&birdList->bird, Pipa, TutupPipa);
+                        ResetSkor();
                         Pipa_berhenti(true);
                         scoreSaved = false;
                         for (int i = 0; i < 3; i++) passedPipe[i] = false;
-                        
-                        // Jangan memulai musik menu di sini, biarkan flag menanganinya
                         menuMusicStarted = false;
                     }
                 }
             }
 
-            // Gambar game
-            DrawBirds(birds, MAX_BIRDS);
+            DrawBirds(birdList);
             Gambar_pipa(Pipa, TutupPipa, score);
 
             DrawText(TextFormat("Score: %d", score), SCREEN_WIDTH / 2 - 60, 10, 30, BLACK);
@@ -188,30 +176,20 @@ int main() {
             } else if (gameOverState == GAME_ACTIVE) {
                 DrawText("Press SPACE to Flap!", 10, 10, 20, DARKGRAY);
                 DrawText("Press P to Pause", 10, 30, 20, DARKGRAY);
+            } else if (gameOverState == GAME_OVER) {
+                DrawText("GAME OVER", SCREEN_WIDTH / 2 - 120, SCREEN_HEIGHT / 2 - 30, 40, RED);
+                DrawText("Press ENTER to Restart", SCREEN_WIDTH / 2 - 140, SCREEN_HEIGHT / 2 + 20, 25, DARKGRAY);
             }
-
-            if (gameOverState == GAME_OVER) {
-                DrawGameOver(SCREEN_WIDTH, SCREEN_HEIGHT, score);
-            }
-        }
-
-        if (tmblpause.isPause) {
-            DrawPauseScreen(&tmblpause);
         }
 
         EndDrawing();
     }
 
-    // Simpan highscore sebelum keluar (jika perlu)
-    SimpanHighscore(); // Fungsi yang diperbarui hanya akan menyimpan jika nilai highscore berbeda
-
-    // Unload dan cleanup
-    UnloadBirds(birds, MAX_BIRDS);
-    UnloadBird(&bird);
+    // Unload resources
+    UnloadBirds(birdList);
     UnloadTexture(cityBg);
-    UnloadSounds();
     CloseAudioDevice();
-    CloseWindow();
+    CloseWindow(); // Close window and OpenGL context
 
     return 0;
 }
